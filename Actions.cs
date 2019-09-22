7541f4207de08ca5f2609380730e2c781eaa5b7a
Pidgey <42968544+LilPidgey@users.noreply.github.com>
2019-09-22 01:02:25 -0400

    Initial Commit

---
+++ Actions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timber
+{
+    class Actions
+    {
+        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int ReleaseDC(IntPtr window, IntPtr dc);
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+        [DllImport("User32.dll")]
+        static extern int SetForegroundWindow(IntPtr point);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+
+
+        //////////////
+        // KEYBOARD //
+        //////////////
+        public const uint VK_LWIN = 0x5B; //Windows Key
+        public const uint VK_ESCAPE = 0x1B; //Escape Key
+        public const uint VK_B = 0x42; //B Key
+
+        ///////////
+        // MOUSE //
+        ///////////
+        public const uint LButton = 1; //Left Mouse Button
+        public const uint VK_RBUTTON = 0x02; //Right Mouse Button
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        ///////////
+        // OTHER //
+        ///////////
+        public const uint WM_KEYDOWN = 0x100; //Key Down
+        public const uint WM_KEYUP = 0x101; //Key Up
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner
+            public int Top;         // y position of upper-left corner
+            public int Right;       // x position of lower-right corner
+            public int Bottom;      // y position of lower-right corner
+        }
+
+        public static void Move(IntPtr windowHandle)
+        {
+            const short SWP_NOMOVE = 0X2;
+            const short SWP_NOSIZE = 1;
+            const short SWP_NOZORDER = 0X4;
+            const int SWP_SHOWWINDOW = 0x0040;
+
+            Point pt = new Point(1, 1);
+            SetWindowPos(windowHandle, 0, pt.X, pt.Y, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+
+        }
+
+        public static Color GetColorAt(int x, int y)
+        {
+            IntPtr desk = GetDesktopWindow();
+            IntPtr dc = GetWindowDC(desk);
+            int a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return Color.FromArgb(255, (a >> 0) & 0xff, (a >> 8) & 0xff, (a >> 16) & 0xff);
+        }
+
+        public static void SendKeyDown(IntPtr windowHandle, Keys key)
+        {
+            SendMessage(windowHandle, WM_KEYDOWN, (int)key, 0);
+        }
+
+        public static void SendKeyss(IntPtr Process, string Message)
+        {
+            SetForegroundWindow(Process);
+            SendKeys.SendWait(Message);
+        }
+
+        public static void MouseClick()
+        {
+            uint X = (uint)Cursor.Position.X;
+            uint Y = (uint)Cursor.Position.Y;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Timber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please choose MS interval \nRecommended 140+ \nInterval: ");
+            int number = Console.Read();
+            Process[] processes = Process.GetProcessesByName("Timberman");
+
+            foreach (Process p in processes)
+            {
+                IntPtr windowHandle = p.MainWindowHandle;
+                Actions.Move(windowHandle);
+                Thread.Sleep(2000);
+                Actions.SendKeyss(windowHandle, "d");
+                Console.WriteLine("tt");
+
+
+                string left = Actions.GetColorAt(483, 467).ToString();
+                string right = Actions.GetColorAt(799, 478).ToString();
+
+                while (true)
+                {
+                    if (left == Actions.GetColorAt(483, 467).ToString())
+                    {
+                        Actions.SendKeyss(windowHandle, "a");
+                        Thread.Sleep(142);
+                    }
+                    else
+                    {
+                        while(right == Actions.GetColorAt(799, 478).ToString())
+                        {
+                            Actions.SendKeyss(windowHandle, "d");
+                            Thread.Sleep(142);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
