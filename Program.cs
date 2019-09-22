fc9b56ab8d7a6360778e6944e18b027fb7b215e1
Pidgey <42968544+LilPidgey@users.noreply.github.com>
2019-09-22 01:08:34 -0400

    Removed debugging code.

--- Program.cs
+++ Program.cs
@@ -24,8 +24,6 @@ namespace Timber
                 Actions.Move(windowHandle);
                 Thread.Sleep(2000);
                 Actions.SendKeyss(windowHandle, "d");
-                Console.WriteLine("tt");
-
 
                 string left = Actions.GetColorAt(483, 467).ToString();
                 string right = Actions.GetColorAt(799, 478).ToString();
